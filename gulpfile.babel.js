cc8f82be14e8ced913620e0f87c3f16c5fa30b34
jrpikong <jr.pikong@gmail.com>
2017-07-13 14:56:55 +0700

    install hot reload with -- npm run dev

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,8 @@ import gulp from "gulp";
 import browserify from "browserify";
 import source from "vinyl-source-stream";
 import sass from "gulp-sass";
+import hmr from 'browserify-hmr';
+import watchify from 'watchify';
 
 gulp.task('default', () => {
     return browserify({
@@ -11,6 +13,26 @@ gulp.task('default', () => {
         .bundle()
         .pipe(source('app.js'))
         .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('hmr', () => {
+    const b = browserify({
+        entries: 'resources/assets/js/app.js',
+        plugin: [hmr, watchify],
+        debug: true
+    })
+
+    b.on('update', bundle);
+    bundle();
+
+    function bundle() {
+        b.bundle()
+            .on('error', err => {
+                util.log("Browserify Error", util.colors.red(err.message))
+            })
+            .pipe(source('app.js'))
+            .pipe(gulp.dest('public/js'));
+    }
 });
 
 gulp.task('sass', function () {
